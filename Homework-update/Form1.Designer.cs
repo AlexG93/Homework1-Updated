7afc1376bcfcbbc74a9df563bffce4b648b21194
AlexG93 <gingalau.alex@gmail.com>
2015-04-26 17:00:05 +0300

    Button 1 click working

--- Homework-update/Form1.Designer.cs
+++ Homework-update/Form1.Designer.cs
@@ -41,10 +41,17 @@
             this.button1.TabIndex = 0;
             this.button1.Text = "button1";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // comboBox1
             // 
             this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "English",
+            "French",
+            "Deutch",
+            "Spanish",
+            "Italian"});
             this.comboBox1.Location = new System.Drawing.Point(91, 124);
             this.comboBox1.Name = "comboBox1";
             this.comboBox1.Size = new System.Drawing.Size(121, 21);
@@ -56,6 +63,7 @@
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(100, 20);
             this.textBox1.TabIndex = 2;
+            this.textBox1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.textBox1_KeyDown);
             this.textBox1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox1_KeyPress);
             // 
             // Form1
--- Homework-update/Form1.cs
+++ Homework-update/Form1.cs
@@ -7,12 +7,13 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Humanizer;
 
 namespace Homework_update
 {
     public partial class Form1 : Form
     {
-        public List<string> lang = new List<string> { "en", "fr", "de", "es","it" };
+        public List<string> language = new List<string> { "en", "fr", "de", "es","it" };
 
         public Form1()
         {
@@ -25,5 +26,35 @@ namespace Homework_update
             //se accepta doar cifre + copy, cut, paste, select all, etc.
             e.Handled = !("0123456789".Contains(e.KeyChar)) && !(char.IsControl(e.KeyChar));
         }
+
+        private void textBox1_KeyDown(object sender, KeyEventArgs e)
+        {
+            //se poate da copy-paste doar la numere intregi
+            if (e.Control && e.KeyCode == Keys.V)
+            {
+                int result;
+                if (!Int32.TryParse(Clipboard.GetText(), out result))
+                {
+                    e.SuppressKeyPress = true;
+                }
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int nr = 0;
+            if (Int32.TryParse(textBox1.Text,out nr))
+            {
+                string result = nr.ToWords(new System.Globalization.CultureInfo(language[comboBox1.SelectedIndex]));
+                MessageBox.Show(result);
+            }
+            else
+            {
+                MessageBox.Show("Enter a digit!");
+            }
+ 
+        }        
     }
-}
+        }
+    
+
